51a595f521a31ffa592b7ccc8225fcb3bdcb4c35
sasisaiunnam <114565859+sasisaiunnam@users.noreply.github.com>
2026-06-16 14:29:06 +0530

    Revert "Revert "Frontend/registerfeature gopal""

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,6 +26,9 @@ import CustomerEditProfile from "./Pages/Customer/CustomerEditProfile";
 import CustomerProducts from "./Pages/Customer/CustomerProducts/CustomerProducts";
 import ProductPage from "./Pages/Customer/CustomerProducts/ProductPage";
 import CustomerCart from "./Pages/Customer/Cart/CustomerCart";
+import CustomerProfile from "./Pages/Customer/CustomerProfiles";
+import OrderList from "./Pages/Customer/CustomerOrder/OrderList";
+
 
 function App() {
 
@@ -62,6 +65,7 @@ function App() {
         <Route path="/customer/cart/:id" element={<CustomerCart/>}/>
          <Route path ="/customer/editprofile" element={<CustomerEditProfile/>}/>
          <Route path ="/customer/editprofile/:id" element={<CustomerEditProfile/>}/>
+         <Route path ="/customer/orderlist" element={<OrderList/>}/>
         </Route>
 
       </Routes>
