eff891b86894aba9118b032ddf5984466250f4ef
sasi unnam <sasisai.unnam@gmail.com>
2026-06-17 10:08:57 +0530

    features updated

--- backend/Controllers/BannerController/Banners.js
+++ backend/Controllers/BannerController/Banners.js
@@ -142,9 +142,15 @@ async function updateBanner(req,res){
         const updateData = { ...req.body };
 
         if (req.files && req.files.length > 0) {
-            const imagePaths = req.files.map(file => file.path);
-            updateData.image = imagePaths;
+            updateData.image = req.files.map(file => `${req.protocol}://${req.get('host')}/${file.path}`);
         }
+
+        if (!updateData.image) {
+            return res.status(400).json({
+                message: "Image is required"
+            })
+        }
+
 
         let banner = await bannerModel.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
 
--- backend/Routes/Orders/ordersRouter.js
+++ backend/Routes/Orders/ordersRouter.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const { getOrders, getAllOrders, updateOrderStatus } = require("../../Controllers/OrdersController/orders");
+const { getOrders, getAllOrders, updateOrderStatus, deleteOrder } = require("../../Controllers/OrdersController/orders");
 const isCustomer = require("../../MiddleWare/customerAuth");
 const isAdmin = require("../../MiddleWare/adminAuth");
 const router = express.Router();
 
-// Customer Routes (Protected by isCustomer token)
 router.get("/getOrders", isCustomer, getOrders);
 
-// Admin Routes (Protected by isAdmin token)
 router.get("/admin/getAllOrders", isAdmin, getAllOrders);
 
-// Admin modifying a specific item uses :id param
 router.put("/admin/updateOrder/:id", isAdmin, updateOrderStatus);
+
+router.delete("/deleteOrder/:id",  deleteOrder);
+
 
 module.exports = router;   
